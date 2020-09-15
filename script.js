b36ce106e6694e395649c142cea9bfb1c70a7f60
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-09-15 20:51:00 +0300

    get a quote via forismatic API

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-/* JS goes here */
+
+/*
+Get a quote via forismatic API - https://forismatic.com/en/api/
+*/
+
+function getQuote() {
+    const proxyURL = "https://cors-anywhere.herokuapp.com/";
+    const apiURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    fetch(proxyURL + apiURL)
+        .then(function(response) {
+           return response.json();
+        })
+        .then(function(quote) {
+            const quoteText = quote.quoteText;
+            const quoteAuthor = quote.quoteAuthor;
+         })        
+        .catch(function(error) {
+            console.info("Something goes wrong...", error, ". Retry");
+            getQuote();
+        });
+}
+
+getQuote();
