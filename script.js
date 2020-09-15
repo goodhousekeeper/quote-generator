f8c8c466b5f729b95ae1cff759cacc5496e03550
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-09-15 21:35:10 +0300

    get a quote via forismatic API

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 Get a quote via forismatic API - https://forismatic.com/en/api/
 */
 
+const quoteContainer = document.getElementById("quote-container");
+const quoteMark = document.getElementById("quote-mark");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const maxQuoteLength = 120;
+const longQuoteClass = "long-quote";
+const longQuoteMarkClass = "long-quote-mark";
+
 function getQuote() {
     const proxyURL = "https://cors-anywhere.herokuapp.com/";
     const apiURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -10,9 +20,17 @@ function getQuote() {
         .then(function(response) {
            return response.json();
         })
-        .then(function(quote) {
-            const quoteText = quote.quoteText;
-            const quoteAuthor = quote.quoteAuthor;
+        .then(function(data) {
+            quoteText.innerText = data.quoteText;
+            quoteAuthor.innerText = data.quoteAuthor === "" ? "Unknown" : data.quoteAuthor;
+            if (data.quoteText.length > maxQuoteLength) {
+                quoteMark.classList.add(longQuoteMarkClass);
+                quoteText.classList.add(longQuoteClass);
+            } else {
+                quoteMark.classList.remove(longQuoteMarkClass);
+                quoteText.classList.remove(longQuoteClass);
+            }
+            
          })        
         .catch(function(error) {
             console.info("Something goes wrong...", error, ". Retry");
